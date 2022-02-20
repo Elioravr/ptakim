927556fb3319fd8e40fa042c085afd3f625c4d2a
Elior Abramoviz Ducker <elior@fb.com>
2022-02-20 21:21:36 +0200

    Add Loading

--- src/App.js
+++ src/App.js
@@ -4,22 +4,31 @@ import AddNewPetekButton from './AddNewPetekButton'
 import PetekList from './PetekList'
 import NewPetekModal from './NewPetekModal'
 import Separator from './Separator'
+import Loading from './Loading'
 import {fetchPetekList} from './apiService';
 import './App.css';
 
 function App() {
   const [isNewPetekModalOpen, setIsNewPetekModalOpen] = useState(false);
   const [list, setList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   const [petekToEdit, setPetekToEdit] = useState(null);
 
   useEffect(() => {
-      fetchPetekList()
-        .then(list => list && setList(list))
-        .catch(e => fetchPetekList().then(list => list && setList(list)));
+    setIsLoading(true);
+    fetchPetekList()
+      .then(list => {
+        list && setList(list);
+        setIsLoading(false);
+      })
+      .catch(e => fetchPetekList().then(list => {
+        list && setList(list);
+        setIsLoading(false);
+      }));
 
-      if (!isNewPetekModalOpen) {
-        setPetekToEdit(null);
-      }
+    if (!isNewPetekModalOpen) {
+      setPetekToEdit(null);
+    }
   }, [isNewPetekModalOpen]);
 
   const editPetek = (petek) => {
@@ -33,9 +42,12 @@ function App() {
         <div className="app-header">
           <span className="logo">Ptakim</span>
         </div>
-        <AddNewPetekButton setIsNewPetekModalOpen={setIsNewPetekModalOpen} />
-        <PetekList list={list} editPetek={editPetek} />
-        <Separator emoji="🤷‍♂️" />
+        {isLoading ? <Loading /> :
+        <>
+          <AddNewPetekButton setIsNewPetekModalOpen={setIsNewPetekModalOpen} />
+          <PetekList list={list} editPetek={editPetek} />
+          <Separator emoji="🤷‍♂️" />
+        </>}
       </div>
       <NewPetekModal isOpen={isNewPetekModalOpen} setIsOpen={setIsNewPetekModalOpen} list={list} petekToEdit={petekToEdit} />
     </div>
---
+++ src/Loading.js
@@ -0,0 +1,34 @@
+import React, {useState, useEffect} from 'react';
+
+let intervalId;
+const Loading = ({text='טוען פאדיחות...', loadingSize='large'}) => {
+  const icons = ['🙊', '🤣', '🤦‍♂️', '🤓', '🤷‍♂️'];
+  const [currentStage, setCurrentStage] = useState(0);
+
+
+  useEffect(() => {
+    clearInterval(intervalId);
+    intervalId = setInterval(() => {
+      let nextStage = currentStage + 1;
+
+      if (nextStage === icons.length) {
+        nextStage = 0;
+      }
+
+      setCurrentStage(nextStage);
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    }
+  });
+
+  return (
+    <div className={`loading-container ${loadingSize}`}>
+      {icons.map((icon, index) => currentStage === index ? <div key={index} className="stage">{icon}</div> : null)}
+      <span className="text">{text}</span>
+    </div>
+  );
+}
+
+export default Loading;
